d5b88ed8a1bd141d2ad43977435d8318e31212cf
맛카치 <mkachi@naver.com>
2018-04-13 10:09:33 +0900

    Add OpenAL.h

--- Galaga/framework/AudioListener.h
+++ Galaga/framework/AudioListener.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#if _WINDOWS
-#include <al.h>
-#include <alc.h>
-#else
-#include <OpenAL/al.h>
-#include <OpenAL/alc.h>
-#endif
+#include "OpenAL.h"
 
 class AudioListener
 {
---
+++ Galaga/framework/OpenAL.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if _WINDOWS
+#include <al.h>
+#include <alc.h>
+#else
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#endif
